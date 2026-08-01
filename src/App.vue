a7df5fd3a1320ca076ea28a4c5021af3c349c6ca
Mio <gusw.0246.lpsyg@gmail.com>
2026-08-02 01:33:19 +0200

    create & delete task

--- src/App.vue
+++ src/App.vue
@@ -7,5 +7,43 @@ interface Todo{
   completed: boolean
 }
 
-type 
+type FilterType = "all" | "active" | "completed"
+
+const newTodoText = ref<string>("")
+const filter = ref<FilterType>("all")
+
+const todos = ref<Todo[]>([
+  {id: 1, text: "sleep", completed: true},
+  {id:2, text: "prepare lunch", completed:false}
+])
+
+// create new task
+const addTodo =(): void =>{
+  const text = newTodoText.value.trim()
+  if(!text) return
+
+  todos.value.push({
+    id: Date.now(),
+    text,
+    completed:false
+  })
+  newTodoText.value = ""
+
+}
+
+// delete task
+
+const removeTodo = (id: number): void =>{
+  todos.value = todos.value.filter(todo => todo.id != id)
+}
+
+const filteredTodos = computed<Todo[]>(() => {
+  if(filter.value === "active"){
+    return todos.value.filter(t=> !t.completed)
+  }
+  if (filter.value === "completed"){
+    return todos.value.filter(t => t.completed)
+  }
+  return todos.value
+}) 
 </script>
